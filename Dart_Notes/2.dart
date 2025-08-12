e71949f40ee66f9bd2452d13650e0287c8b5c720
Yashvigor <yashvigor.ce@gmail.com>
2025-08-12 19:16:44 +0530

    Create 2.dart

---
+++ Dart_Notes/2.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+void main(){
+  // print('Welcome to Dart');
+  // stdout.write('hello\n');
+  // var name=stdin.readLineSync();
+  // print("Welcome, $name");
+  //
+   var me = new Human(); //creating an object , new is optional 
+}
+class Human{
+    Human(); //default constructor
+  
+}
+
