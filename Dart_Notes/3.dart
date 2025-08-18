8858eacf1203e66b9083a636898bd63ad95c8f90
Yashvigor <yashvigor.ce@gmail.com>
2025-08-18 17:33:59 +0530

    Create 3.dart

---
+++ Dart_Notes/3.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+void main(){
+
+var yashvi= new Human(); //invokes constructor of the class
+
+}
+class Human {
+  // '{': scope resolution , Human: Identifier
+  Human();
+}
+
