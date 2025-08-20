9f0170de7d433ced8a6daa34a6bf27485e257c28
Yashvigor <yashvigor.ce@gmail.com>
2025-08-20 18:52:13 +0530

    Create OutlinedButton.dart

---
+++ Notes/OutlinedButton.dart
@@ -0,0 +1,15 @@
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+        body: OutlinedButton(onPressed: (){
+          print('Clicked');
+        }, child: Text('Outlined Button'))
+    );
+  }
+}
