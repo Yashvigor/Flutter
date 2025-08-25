a94653db05176855efdbfe0242e13ad05ae28fbc
Yashvigor <yashvigor.ce@gmail.com>
2025-08-25 21:39:58 +0530

    Create Card-Widget.dart

---
+++ Notes/Card-Widget.dart
@@ -0,0 +1,26 @@
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+
+      ),
+      body: Center(
+        child: Card(
+          shadowColor: Colors.blue,
+          elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Hello World',style: TextStyle(fontSize: 21),),
+            )
+
+
+
+        ),
+      ),
+    );
+  }
+}
+
